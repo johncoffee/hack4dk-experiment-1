d4d56be1a9150c92539ad74ae1643ddbfe43c89e
Julian <julian@contingent.dk>
2015-10-02 22:28:15 +0200

    sdfsdf

---
+++ files.js
@@ -0,0 +1,16 @@
+var walk    = require('walk');
+var files   = [];
+
+// Walker options
+var walker  = walk.walk('./assets/images', { followLinks: false });
+
+walker.on('file', function(root, stat, next) {
+    // Add this file to the list of files
+    files.push(root + '/' + stat.name);
+    next();
+});
+
+walker.on('end', function() {
+    console.log(files);
+});
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,41 @@
-console.log("init");
 
+function append() {
+    var images = [ './assets/images/16213259323_7dba16b75c_z.jpg',
+        './assets/images/16443294914_32213cceff_z.jpg',
+        './assets/images/16656845429_425b8305e4_z.jpg',
+        //'./assets/images/16841568492_3684de3c87_z.jpg',
+        //'./assets/images/16880711829_4c9a22552c_z.jpg',
+        //'./assets/images/17066613675_1b479b849e_z.jpg',
+        //'./assets/images/17608847822_bd9fe1c35f_z.jpg',
+        //'./assets/images/21581261632_d0d880ac5a_z.jpg',
+        //'./assets/images/CRW_2900.jpg',
+        //'./assets/images/KMS3025.jpg',
+        //'./assets/images/img0111.jpg',
+        //'./assets/images/img0145.jpg',
+        //'./assets/images/img0146.jpg',
+        //'./assets/images/img0158.jpg',
+        //'./assets/images/img0259.jpg',
+        //'./assets/images/img0264.jpg',
+        //'./assets/images/kms7479.jpg',
+        //'./assets/images/tumblr_inline_nmqucnBpHF1t8luy9_500.jpg',
+        //'./assets/images/tumblr_inline_nmqudsB1Dd1t8luy9_500.jpg',
+        './assets/images/tumblr_inline_nmquj7vGhT1t8luy9_540.jpg' ];
+
+
+    var tplNode = document.querySelector("#imgtpl");
+
+    var tplCloneNode = document.importNode(tplNode.content, true);
+    
+    var html = "";
+    images.forEach(function (item) {
+        html += tplCloneNode.innerHTML
+            .replace("{{src}}", item);
+    });
+    console.log(html);
+    
+    document.querySelector("#images_con").innerHTML = html;
+}
+
+append();
 
 
