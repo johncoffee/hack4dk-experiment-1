007d9a8b5595219822615d5c6548229d20e1d673
Julian <julian@contingent.dk>
2015-10-02 23:33:50 +0200

    instagram stuff

---
+++ scripts/OAuthService.js
@@ -0,0 +1,115 @@
+var AuthService = (function () {
+    var that = {};
+
+    if (!localStorage.instagramApiKey) {
+        console.warn('missing API key in instagramApiKey')
+    }
+    OAuth.initialize(localStorage.instagramApiKey);
+    
+    var _fields = ['username', 'userID'];
+    
+    _fields.forEach(function(fieldName) {
+        Object.defineProperty(that, fieldName, {
+            get: function() {
+                return sessionStorage.getItem(fieldName);
+            },
+            set: function(value) {
+                if (typeof value !== 'undefined') {
+                    sessionStorage.setItem(fieldName, value);
+                }
+                else {
+                    sessionStorage.removeItem(fieldName);
+                }
+            },
+        });
+    });
+
+    // dev stuff
+    that.loginFake = function() {
+        that.authenticated = true;
+        that.username = "julian";
+        that.userID = 5;
+    };
+    
+    Object.defineProperty(that, 'authenticated', {
+        get: function() {
+            return (sessionStorage.getItem('authenticated') == 1);
+        },
+        set: function(value) {
+            if (value) {
+                sessionStorage.setItem('authenticated', 1);
+            }
+            else {
+                that.deAuth();
+            }
+        },
+    });
+    
+    that.deAuth = function() {
+        sessionStorage.removeItem('authenticated');
+        _fields.forEach(function(item) {
+            sessionStorage.removeItem(item);
+        });
+    };
+
+    that.authAndRedirect = function(provider, route) {
+        OAuth.redirect(provider, route);
+    };
+    
+    that.onRedirectCallback = function (callback) {
+        OAuth.callback(function(err, res) {
+            if (err) {
+                console.warn(err);
+                //document.getElementById('nice').innerHTML = err;
+            }
+            else {
+                callback(res);
+            }
+            //console.debug(res);
+        });
+    };
+
+    that.openInstagramPopup = function() {
+        OAuth.popup('instagram')
+        .done(function(result) {
+            console.log(result)
+            // do some stuff with result
+            fetchTags(result);
+        }).fail(function (err) {
+            console.warn(err);
+            //document.getElementById("nice").innerHTML = err;
+        });
+    };
+    
+    //console.log(OAuth);
+    that.openAuthPopup2 = function() {
+        OAuth.popup('facebook')
+        //OAuth.redirect('facebook', {}, 
+            .done(function(result) {
+
+                result.get('/me')
+                    .done(function (me) {
+                        console.info(me);
+                    })
+                    .fail(function (err) {
+                        console.warn(err);
+                        document.getElementById("nice").innerHTML = err;
+                    });
+            })
+            .fail(function (err) {
+                console.warn(err);
+                document.getElementById("nice").innerHTML = err;
+            });  
+    };
+    
+    try {
+        if (sessionStorage.wasLoggedIn != null) {
+            //that.openAuthPopup();
+        }   
+    }
+    catch (e) {
+        console.warn(e);
+    }
+
+    return that;
+})();
--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,74 @@
-(function () {
+$(function () {
 
 
-    function appendOldPeople() {
-        var images = [ './assets/images/16213259323_7dba16b75c_z.jpg',
-            './assets/images/16443294914_32213cceff_z.jpg',
-            './assets/images/16656845429_425b8305e4_z.jpg',
-            './assets/images/16841568492_3684de3c87_z.jpg',
-            './assets/images/16880711829_4c9a22552c_z.jpg',
-            './assets/images/17066613675_1b479b849e_z.jpg',
-            './assets/images/17608847822_bd9fe1c35f_z.jpg',
-            './assets/images/21581261632_d0d880ac5a_z.jpg',
-            './assets/images/CRW_2900.jpg',
-            './assets/images/KMS3025.jpg',
-            './assets/images/img0111.jpg',
-            './assets/images/img0145.jpg',
-            './assets/images/img0146.jpg',
-            './assets/images/img0158.jpg',
-            './assets/images/img0259.jpg',
-            './assets/images/img0264.jpg',
-            './assets/images/kms7479.jpg',
-            './assets/images/tumblr_inline_nmqucnBpHF1t8luy9_500.jpg',
-            './assets/images/tumblr_inline_nmqudsB1Dd1t8luy9_500.jpg',
-            './assets/images/tumblr_inline_nmquj7vGhT1t8luy9_540.jpg'
-        ];
-
-
-        var tplNode = document.querySelector("#imgtpl");
-
-
-        var html = "";
-        images.forEach(function (item) {
-            html += tplNode.innerHTML
-                .replace("{{src}}", item);
-        });
-
-        document.querySelector("#images_con").innerHTML += html;
-    }
-
-    appendOldPeople();
+    AuthService.openInstagramPopup();
     
-})();
+    appendOldPeople();
+});
 
 
+function fetchTags(instagramObj) {
+    ///tags/{{tag-name}}/media/recent
+    
+    //var req = instagramObj.get("https://api.instagram.com/v1/tags/cat/media/recent");
+    var req = instagramObj.get("https://api.instagram.com/v1/users/self/feed");
+    
+    req.done(function () {
+        var json = req.responseJSON;
+        console.debug(json);
+        var urls = [];
+
+        json.data.forEach(function (item) {
+            urls.push(item.images.standard_resolution.url)
+        })             
+        
+        instaImgs(urls);
+    });
+}
+
+function instaImgs(images) {
+    var tplNode = document.querySelector("#imgtpl");
+    var html = "";
+    images.forEach(function (item) {
+        html += tplNode.innerHTML
+            .replace("{{src}}", item);
+    });
+
+    document.querySelector("#images_con2").innerHTML += html;
+}
+
+function appendOldPeople() {
+    var images = [ './assets/images/16213259323_7dba16b75c_z.jpg',
+        './assets/images/16443294914_32213cceff_z.jpg',
+        './assets/images/16656845429_425b8305e4_z.jpg',
+        './assets/images/16841568492_3684de3c87_z.jpg',
+        './assets/images/16880711829_4c9a22552c_z.jpg',
+        './assets/images/17066613675_1b479b849e_z.jpg',
+        './assets/images/17608847822_bd9fe1c35f_z.jpg',
+        './assets/images/21581261632_d0d880ac5a_z.jpg',
+        './assets/images/CRW_2900.jpg',
+        './assets/images/KMS3025.jpg',
+        './assets/images/img0111.jpg',
+        './assets/images/img0145.jpg',
+        './assets/images/img0146.jpg',
+        './assets/images/img0158.jpg',
+        './assets/images/img0259.jpg',
+        './assets/images/img0264.jpg',
+        './assets/images/kms7479.jpg',
+        './assets/images/tumblr_inline_nmqucnBpHF1t8luy9_500.jpg',
+        './assets/images/tumblr_inline_nmqudsB1Dd1t8luy9_500.jpg',
+        './assets/images/tumblr_inline_nmquj7vGhT1t8luy9_540.jpg'
+    ];
+
+
+    var tplNode = document.querySelector("#imgtpl");
+
+
+    var html = "";
+    images.forEach(function (item) {
+        html += tplNode.innerHTML
+            .replace("{{src}}", item);
+    });
+
+    document.querySelector("#images_con").innerHTML += html;
+}
